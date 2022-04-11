5268724c6bdf66f60948a81bfa6972ad2a2710f4
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-11 02:51:15 +0000

    Calculate the average total duration of assistance requests for each cohort

--- 4_queries/10_most_confusing_assignments.sql
+++ 4_queries/10_most_confusing_assignments.sql
@@ -2,9 +2,8 @@ SELECT assignments.id,
   assignments.name,
   assignments.day,
   assignments.chapter,
-  count(assignment_id) as total_requests 
+  count(assignment_id) as total_requests
 FROM assistance_requests
- JOIN assignments ON assignment_id = assignments.id
+  JOIN assignments ON assignment_id = assignments.id
 GROUP BY assignments.id
-  ORDER BY total_requests DESC
- 
+ORDER BY total_requests DESC;
