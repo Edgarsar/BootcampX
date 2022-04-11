3b541246ed2a17f3f56636d282d0fd5a54a2ba13
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-11 03:04:34 +0000

    Get the name of all teachers that performed an assistance request during a cohort

--- 4_queries/12_name_of_teachers_that_assisted.sql
+++ 4_queries/12_name_of_teachers_that_assisted.sql
@@ -1,7 +1,8 @@
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+SELECT DISTINCT teachers.name as teacher,
+  cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
-ORDER BY teachers.name
+ORDER BY teachers.name;
